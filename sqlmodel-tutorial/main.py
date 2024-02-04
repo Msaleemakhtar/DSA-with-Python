2c1027d0ad113e8765bbc84b272c38fc70940be3
Msaleem <saleemakhtar864@gmail.com>
2024-02-04 19:10:01 +0500

    worked on dsa

--- sqlmodel-tutorial/main.py
+++ sqlmodel-tutorial/main.py
@@ -2,10 +2,12 @@ from typing import Optional
 
 from sqlmodel import Field, SQLModel, create_engine, Session,select
 
-DATA_BASE_URL=""
 
 
-
+class Team(SQLModel, table= True):
+    id:Optional[int] = Field(default=None, primary_key=True)
+    name :str = Field(index=True)
+    headdquarters : str
 
 
 class Hero(SQLModel, table=True):
@@ -13,6 +15,8 @@ class Hero(SQLModel, table=True):
     name: str = Field(index=True)
     secret_name: str
     age: Optional[int] = Field(default=None, index=True)
+
+    team_id: Optional[int]= Field(default=None, foreign_key="team.id")
 
 
 
@@ -23,78 +27,52 @@ engine = create_engine(DATA_BASE_URL, echo=True)
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
-
-# Code above omitted 👆
-
-def create_heroes():
-    hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
-    hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
-    hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
-    hero_4 = Hero(name="Tarantula", secret_name="Natalia Roman-on", age=32)
-    hero_5 = Hero(name="Black Lion", secret_name="Trevor Challa", age=35)
-    hero_6 = Hero(name="Dr. Weird", secret_name="Steve Weird", age=36)
-    hero_7 = Hero(name="Captain North America", secret_name="Esteban Rogelios", age=93)
-
+def create_hero():
     with Session(engine) as session:
-        session.add(hero_1)
-        session.add(hero_2)
-        session.add(hero_3)
-        session.add(hero_4)
-        session.add(hero_5)
-        session.add(hero_6)
-        session.add(hero_7)
+        team_preventer = Team(name="preventer", headdquarters="sharp Tower")
+        team__z_force = Team(name="z force", headdquarters="sister Margarate force")
 
+        # session.add(team_preventer)
+        # session.add(team__z_force)
+        # session.commit()
+
+        hero_deadpond = Hero(name="Deadpond", secret_name="Dive wilson", team_id=team_preventer.id)
+        hero_rusty_man = Hero(name="RustyMan", secret_name="Tommy Sharp", age=48, team_id=team__z_force.id)
+        hero_spider_man= Hero(name="SpiderBoy", secret_name="Pedro parker", age=32)
+
+        # session.add(hero_deadpond)
+        # session.add(hero_rusty_man)
+        # session.add(hero_spider_man)
+
+        # session.commit()
+
+        # session.refresh(hero_deadpond)
+        # session.refresh(hero_rusty_man)
+        # session.refresh(hero_spider_man)
+
+        # print("hero_deadpond", hero_deadpond)
+        # print("rudty_hero", hero_rusty_man)
+        # print("spider_hero", hero_spider_man)
+
+        hero_spider_man.team_id = team_preventer.id
+        session.add(hero_spider_man)
         session.commit()
+        session.refresh(hero_spider_man)
 
 
-def update_hero():
+def select_heros():
     with Session(engine) as session:
-        statement = select(Hero).where(Hero.id == 1)
+        statement = select(Hero, Team).join(Team, isouter=True)
         results = session.exec(statement)
-
-        hero_1 = results.one()
-        print("-------------------------------")
-        print("Before update; ", hero_1)
-
-        hero_1.age = 45
-        hero_1.name= "saram ali"
-        session.add(hero_1)
-        session.commit()
-        session.refresh(hero_1)
-        print("-------------------------------")
-
-        print("After update; ", hero_1)
-
-
-def delete_hero():
-    with Session(engine) as session:
-        statement = select(Hero).where(Hero.id == 1)
-        reults= session.exec(statement)
-        hero_1 = reults.one()
-        print("delete hero: ", hero_1)
-
-        session.delete(hero_1)
-        session.commit()
-        session.refresh(hero_1)
-
-        print("hereo deleted : ", hero_1)
-
-
-
-
-def select_heroes():
-    with Session(engine) as session:
-        statement = select(Hero).where(Hero.id == 1)
-        results = session.exec(statement)
-        print(results.first())
+        heros = results.all()
+        print(heros)
 
 
 def main():
     # create_db_and_tables()
-    # create_heroes()
-    select_heroes()
-    # update_hero()
-    # delete_hero()
+    create_hero()
+    # select_heros()
+ 
 
 if __name__ == "__main__":
     main()
