06e536c0844d9e4326f1ff49a19e830d87680e30
Msaleem <saleemakhtar864@gmail.com>
2024-02-05 20:25:42 +0500

    code added

--- sqlmodel-tutorial/main.py
+++ sqlmodel-tutorial/main.py
@@ -8,82 +8,106 @@ from os import getenv
 
 
 
-load_dotenv(find_dotenv)
+load_dotenv(find_dotenv())
 
 
+Postgess_url = getenv("DATA_BASE_URL")
 
-class Team(SQLModel, table= True):
-    id:Optional[int] = Field(default=None, primary_key=True)
-    name :str = Field(index=True)
-    headdquarters : str
-    heroes:List["Hero"] = Relationship(back_populates="team")
+class HeroTeamLink(SQLModel, table=True):
+    team_id: Optional[int] = Field(
+        default=None, foreign_key="team.id", primary_key=True
+    )
+    hero_id: Optional[int] = Field(
+        default=None, foreign_key="hero.id", primary_key=True
+    )
+    is_training: bool = False
 
+    team: "Team" = Relationship(back_populates="hero_links")
+    hero: "Hero" = Relationship(back_populates="team_links")
+
+class Team(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    headquarters: str
+    hero_links: List[HeroTeamLink] = Relationship(back_populates="team")
+    
+    
 
 class Hero(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(index=True)
     secret_name: str
     age: Optional[int] = Field(default=None, index=True)
-
-    team_id: Optional[int]= Field(default=None, foreign_key="team.id")
-
-    team:Optional[Team] = Relationship(back_populates="heroes")
-
+    team_links: List[HeroTeamLink] = Relationship(back_populates="hero")
+    
 
 
 Postgess_url = getenv("DATA_BASE_URL")
 engine = create_engine(Postgess_url, echo=True)
 
+# Code above omitted 👆
+
+def create_heroes():
+    with Session(engine) as session:
+        team_preventers = Team(name="Preventers", headquarters="Sharp Tower")
+        team_z_force = Team(name="Z-Force", headquarters="Sister Margaret's Bar")
+
+        hero_deadpond = Hero(
+            name="Deadpond",
+            secret_name="Dive Wilson",
+            teams=[team_z_force, team_preventers],
+        )
+        hero_rusty_man = Hero(
+            name="Rusty-Man",
+            secret_name="Tommy Sharp",
+            age=48,
+            teams=[team_preventers],
+        )
+        hero_spider_boy = Hero(
+            name="Spider-Boy", secret_name="Pedro Parqueador", teams=[team_preventers]
+        )
+        session.add(hero_deadpond)
+        session.add(hero_rusty_man)
+        session.add(hero_spider_boy)
+        session.commit()
+
+        session.refresh(hero_deadpond)
+        session.refresh(hero_rusty_man)
+        session.refresh(hero_spider_boy)
+
+        print("Deadpond:", hero_deadpond)
+        print("Deadpond teams:", hero_deadpond.teams)
+        print("Rusty-Man:", hero_rusty_man)
+        print("Rusty-Man Teams:", hero_rusty_man.teams)
+        print("Spider-Boy:", hero_spider_boy)
+        print("Spider-Boy Teams:", hero_spider_boy.teams)
+
+# Code below omitted 👇
+# Code above omitted 👆
+
+def update_heroes():
+    with Session(engine) as session:
+        hero_spider_boy = session.exec(
+            select(Hero).where(Hero.name == "Spider-Boy")
+        ).one()
+        team_z_force = session.exec(select(Team).where(Team.name == "Z-Force")).one()
+
+        hero_spider_boy.teams.remove(team_z_force)
+        session.add(team_z_force)
+        session.commit()
+
+        print("Updated Spider-Boy's Teams:", hero_spider_boy.teams)
+        print("Z-Force heroes:", team_z_force.heroes)
+
+# Code below omitted 👇
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
-def create_hero():
-    with Session(engine) as session:
-        team_preventer = Team(name="preventer", headdquarters="sharp Tower")
-        team__z_force = Team(name="z force", headdquarters="sister Margarate force")
-
-        # session.add(team_preventer)
-        # session.add(team__z_force)
-        # session.commit()
-
-        hero_deadpond = Hero(name="Deadpond", secret_name="Dive wilson", team_id=team_preventer.id)
-        hero_rusty_man = Hero(name="RustyMan", secret_name="Tommy Sharp", age=48, team_id=team__z_force.id)
-        hero_spider_man= Hero(name="SpiderBoy", secret_name="Pedro parker", age=32)
-
-        # session.add(hero_deadpond)
-        # session.add(hero_rusty_man)
-        # session.add(hero_spider_man)
-
-        # session.commit()
-
-        # session.refresh(hero_deadpond)
-        # session.refresh(hero_rusty_man)
-        # session.refresh(hero_spider_man)
-
-        # print("hero_deadpond", hero_deadpond)
-        # print("rudty_hero", hero_rusty_man)
-        # print("spider_hero", hero_spider_man)
-
-        hero_spider_man.team_id = team_preventer.id
-        session.add(hero_spider_man)
-        session.commit()
-        session.refresh(hero_spider_man)
-
-
-def select_heros():
-    with Session(engine) as session:
-        statement = select(Hero, Team).join(Team, isouter=True)
-        results = session.exec(statement)
-        heros = results.all()
-        print(heros)
-
 
 def main():
-    # create_db_and_tables()
-    create_hero()
-    # select_heros()
  
+  update_heroes()
 
 if __name__ == "__main__":
     main()
