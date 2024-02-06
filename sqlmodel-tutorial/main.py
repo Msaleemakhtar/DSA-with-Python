e018b08fe32076b51d34b4db9651d03bb9d1fc3c
Msaleem <saleemakhtar864@gmail.com>
2024-02-06 23:09:49 +0500

    updated

--- sqlmodel-tutorial/main.py
+++ sqlmodel-tutorial/main.py
@@ -1,21 +1,66 @@
 from typing import Optional, List
-
-from sqlmodel import Field, SQLModel, create_engine, Session,select, Relationship
-
-from dotenv import load_dotenv, find_dotenv
 from os import getenv
+from dotenv import load_dotenv, find_dotenv
+
+from fastapi import FastAPI
+from sqlmodel import Field, SQLModel, create_engine, Session,select, Relationship
 
 
 
 
 load_dotenv(find_dotenv())
-
-
 postgres_url = getenv("DATA_BASE_URL")
 
 
+app:FastAPI = FastAPI()
+
+
+class Hero(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    secret_name: str
+    age: Optional[int] = Field(default=None, index=True)
+
+
+class HeroCreate(SQLModel):
+    name: str
+    secret_name: str
+    age: Optional[int] = None
+
+
+class HeroRead(SQLModel):
+    id: int
+    name: str
+    secret_name: str
+    age: Optional[int] = None
 
 engine = create_engine(postgres_url, echo=True) 
 
-# Code above omitted 👆
 
+
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
+@app.post("/heroes/", response_model=HeroRead)
+def create_hero(hero: HeroCreate):
+    with Session(engine) as session:
+        db_hero = Hero.model_validate(hero)
+        session.add(db_hero)
+        session.commit()
+        session.refresh(db_hero)
+        return db_hero
+
+
+@app.get("/heroes/", response_model=List[HeroRead])
+def read_heroes():
+    with Session(engine) as session:
+        heroes = session.exec(select(Hero)).all()
+        return heroes
